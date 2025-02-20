7d23c9781e982644e18c08d111e40c6522b14bb5
Tommy <tommycho1223@gmail.com>
2025-02-20 14:01:07 -0800

    Step 2.1 lab 7

--- map.js
+++ map.js
@@ -10,3 +10,22 @@ const map = new mapboxgl.Map({
     minZoom: 5, // Minimum allowed zoom
     maxZoom: 18 // Maximum allowed zoom
 });
+
+map.on('load', () => {
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+      });
+
+    // Add bike lanes visualization layer
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': 'green',
+            'line-width': 3,
+            'line-opacity': 0.4
+        }
+    });
+});
