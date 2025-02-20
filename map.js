790a4985ab31c6ad20af724cf1cd9d7f8bcad649
Tommy <tommycho1223@gmail.com>
2025-02-20 14:11:46 -0800

    Step 2.3 lab 7

--- map.js
+++ map.js
@@ -17,15 +17,33 @@ map.on('load', () => {
         data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
       });
 
-    // Add bike lanes visualization layer
+    // Add Cambridge bike lane data source
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://data.cambridgema.gov/resource/tdj5-5ph6.geojson' // Example Cambridge bike lane dataset
+    });
+
+    // Add Boston bike lanes layer
     map.addLayer({
-        id: 'bike-lanes',
+        id: 'bike-lanes-boston',
         type: 'line',
         source: 'boston_route',
         paint: {
             'line-color': '#32D400',  // A bright green using hex code
             'line-width': 5,          // Thicker lines
             'line-opacity': 0.6       // Slightly less transparent
-          }          
+          }
+    });
+    
+    // Add Cambridge bike lanes layer
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': '#32D400',  // A bright green using hex code
+            'line-width': 5,          // Thicker lines
+            'line-opacity': 0.6       // Slightly less transparent
+          }
     });
 });
